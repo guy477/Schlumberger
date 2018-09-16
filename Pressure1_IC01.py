24b313349d78f886344c90efc139636d9a041fec
Guy477 <tpratt021@gmail.com>
2018-09-15 22:23:19 -0500

    major commit

---
+++ Pressure1_IC01.py
@@ -0,0 +1,47 @@
+import datetime
+import numpy as np
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+inp = np.loadtxt('data/IC01_PRESSURE1.txt')
+print(inp)
+
+z = []
+xchart = []
+ychart = []
+
+for i in range(288):
+    ychart.append("Minute: " + str(i))
+print(len(inp))
+
+for i in range(int(len(inp)/288 + 1)):
+    xchart.append("Day: " + str(i))
+for i in range(len(xchart)):
+    new_row = []
+    for g in range(288):
+        if not(i*288 + g+1 > len(inp)):
+            new_row.append((np.log(inp[i*288 + g]) - np.log(2470.357378))/np.log(2470.357378))
+        else:
+            new_row.append("no data")
+    z.append(list(new_row))
+
+print(len(z[-1]))
+print(len(z[0]))
+print(len(z))
+data = [
+    go.Heatmap(
+        z=z,
+        y=xchart,
+        x=ychart,
+        colorscale='Greys',
+    )
+]
+
+layout = go.Layout(
+    title='Pressure 1 in psia (IC01)',
+    xaxis = dict(ticks='', nticks=36),
+    yaxis = dict(ticks='' )
+)
+
+fig = go.Figure(data=data, layout=layout)
+py.plot(fig, filename='pressure1-heatmap-IC01')
---
+++ Water_cut_IC01.py
@@ -0,0 +1,47 @@
+import datetime
+import numpy as np
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+inp = np.loadtxt('data/IC01_WATER_CUT.txt')
+print(inp)
+
+z = []
+xchart = []
+ychart = []
+
+for i in range(288):
+    ychart.append("Minute: " + str(i))
+print(len(inp))
+
+for i in range(int(len(inp)/288 + 1)):
+    xchart.append("Day: " + str(i))
+for i in range(len(xchart)):
+    new_row = []
+    for g in range(288):
+        if not(i*288 + g+1 > len(inp)):
+            new_row.append((np.log(inp[i*288 + g]) - np.log(33.20370257))/np.log(33.20370257))
+        else:
+            new_row.append("no data")
+    z.append(list(new_row))
+
+print(len(z[-1]))
+print(len(z[0]))
+print(len(z))
+data = [
+    go.Heatmap(
+        z=z,
+        y=xchart,
+        x=ychart,
+        colorscale='Greys',
+    )
+]
+
+layout = go.Layout(
+    title='Ratio of Water in % (IC01)',
+    xaxis = dict(ticks='', nticks=36),
+    yaxis = dict(ticks='' )
+)
+
+fig = go.Figure(data=data, layout=layout)
+py.plot(fig, filename='water_cut_heatmap-IC01')
